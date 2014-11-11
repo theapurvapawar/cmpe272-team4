4710135358507e0c21b31f6d38a61fd464e12ad6
Apurva Pawar <theapurvapawar@gmail.com>
2014-11-10 18:06:55 -0800

    work on mongoose in progress

--- CMPE272/routes/api.js
+++ CMPE272/routes/api.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 router.get('/', function (req, res) {  
 	  res.send('CMPE 272 API Base. Nothing to be seen here. API page works.');  
 });
+
+
+mongoose.connect('mongodb://localhost/vta');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+});
+
+var VTAStopsSchema = new mongoose.Schema(
+		{},
+		{
+			collection: 'stops'
+		}
+		);
+
+var Stops = db.model('stops', VTAStopsSchema);
+
+router.get('/stop_id/:id', function (req, res){
+	  return Stops.find({'RTI_STOP' : req.params.id}, function (err, singleStop) {
+	    if (!err) {
+	      return res.send(singleStop);
+	    } else {
+	      return console.log(err);
+	    }
+	  });
+	});
+
 
 module.exports = router;
 
