2d920426d676a0fde9fa44069491f22d0abaf5e6
akshaybhasme <akshaybhasme@10.185.220.39>
2014-11-19 15:09:30 -0800

    findOne in get stop from ID API

--- CMPE272/routes/api.js
+++ CMPE272/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 router.get('/', function (req, res) {  
-	  res.send('CMPE 272 API Base. Nothing to be seen here. API page works.');  
+	res.send('CMPE 272 API Base. Nothing to be seen here. API page works.');  
 });
 
 
@@ -11,51 +11,50 @@ mongoose.connect('mongodb://IbmCloud_tquh43sj_t83fg534_4jq4ihh5:D_ShaT44kapjLq0U
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-});
+db.once('open', function callback () {});
 
 var VTAStopsSchema = new mongoose.Schema(
 		{},
 		{
 			collection: 'stops'
 		}
-		);
+);
 
 var Stops = db.model('stops', VTAStopsSchema);
 
 router.get('/stops/stopId/:id', function (req, res){
-	  return Stops.find({'RTI_STOP':parseInt(req.params.id)}, function (err, singleStop) {
-	    if (!err) {
-	      return res.send(singleStop);
-	    } else {
-	      return console.log(err);
-	    }
-	  });
+	return Stops.findOne({'RTI_STOP':parseInt(req.params.id)}, function (err, singleStop) {
+		if (!err) {
+			return res.send(singleStop);
+		} else {
+			return console.log(err);
+		}
 	});
+});
 
 
 router.get('/stops/geoRange', function (req, res){
-	  return Stops.find(
-			  {
-				  'LONG_':
-				  {
-					  '$gt': parseFloat(req.query.long1),
-					  '$lt': parseFloat(req.query.long2)
-				  },
-			  	  'LAT':
-			  	  {
-					  '$gt': parseFloat(req.query.lat1),
-					  '$lt': parseFloat(req.query.lat2)
-			  	  }
-			  }
-			  , function (err, singleStop) {
-	    if (!err) {
-	      return res.send(singleStop);
-	    } else {
-	      return console.log(err);
-	    }
-	  });
-	});
+	return Stops.find(
+			{
+				'LONG_':
+				{
+					'$gt': parseFloat(req.query.long1),
+					'$lt': parseFloat(req.query.long2)
+				},
+				'LAT':
+				{
+					'$gt': parseFloat(req.query.lat1),
+					'$lt': parseFloat(req.query.lat2)
+				}
+			}
+			, function (err, singleStop) {
+				if (!err) {
+					return res.send(singleStop);
+				} else {
+					return console.log(err);
+				}
+			});
+});
 
 module.exports = router;
 
