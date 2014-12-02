0b779a78351cca415ad51c5c2ec8bdc501391964
akshaybhasme <akshaybhasme@10.0.0.21>
2014-12-01 23:55:42 -0800

    search in header

--- CMPE272/public/js/ctrls/base.js
+++ CMPE272/public/js/ctrls/base.js
@@ -1,36 +1,11 @@
 cmpe.controller('baseCtrl', function($scope, $rootScope, $http){
 	$rootScope.map = { center: { latitude: 37.6, longitude: -122.2 }, zoom: 10 };
 	
-	$scope.marker = {
-			id: 0,
-			coords: {
-				latitude: 37.336,
-				longitude: -121.884
-			},
-			options: { draggable: true },
-			events: {
-				dragend: function (marker, eventName, args) {
-					$log.log('marker dragend');
-					var lat = marker.getPosition().lat();
-					var lon = marker.getPosition().lng();
-					$log.log(lat);
-					$log.log(lon);
-
-					$scope.marker.options = {
-							draggable: true,
-							labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-							labelAnchor: "100 0",
-							labelClass: "marker-labels"
-					};
-				}
-			}
-	};
-	
 	$scope.markers = [];
 	
 	$rootScope.getApartmentsNear = function(lat, long){
 		var diff = 0.01;
-	    $scope.panTo(lat, long);
+	    $scope.panTo(lat, long, 14);
 		$http.get('/api/apartments/geoRange?lat1='+(lat-diff)+'&long1='+(long-diff)+'&lat2='+(lat+diff)+'&long2='+(long+diff)).success(function(data){
 			var pad = $scope.markers.length+1;
 			$scope.markers = [];
@@ -46,10 +21,10 @@ cmpe.controller('baseCtrl', function($scope, $rootScope, $http){
 		});
 	};  
 	
-	$scope.panTo = function(lat, lng){
+	$scope.panTo = function(lat, lng, zoom){
 		$rootScope.map.center.latitude = lat;
 		$rootScope.map.center.longitude = lng;
-		$rootScope.map.zoom = 14;
+		$rootScope.map.zoom = zoom;
 	};
 	
 });
