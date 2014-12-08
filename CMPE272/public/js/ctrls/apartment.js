65ad64f34db716cc16b2ccbbfbc93cf259059978
akshaybhasme <akshaybhasme@10.0.0.21>
2014-12-07 23:55:29 -0800

    post a listing form

--- CMPE272/public/js/ctrls/apartment.js
+++ CMPE272/public/js/ctrls/apartment.js
@@ -1,6 +1,34 @@
-cmpe.controller('apartmentCtrl', function($scope, $stateParams, $http){
+cmpe.controller('apartmentCtrl', function($scope, $stateParams, $http, $modal){
+
 	var url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+$stateParams.placeID+'&key=AIzaSyAUwMUP0T2KT_aZaJK5ukT6VZoX6rOpUgo'
+
 	$http.post('/api/forwardRequest', { url : url}).success(function(data){
 		$scope.apartment = data.result;
 	});
+
+	$scope.postListing = function(){
+		var modalInstance = $modal.open({
+			templateUrl: 'views/modals/postlisting.html',
+			controller: 'postListingCtrl',
+			//size: 'sm',
+			resolve: {
+				apartment: function () {
+					return $scope.apartment;
+				}
+			}
+		});
+
+		modalInstance.result.then(function (selectedItem) {
+			//$scope.selected = selectedItem;
+		}, function () {
+			//$log.info('Modal dismissed at: ' + new Date());
+		});
+	};
+
+
+});
+
+cmpe.controller('postListingCtrl', function($scope, $http, $modalInstance, apartment){
+	$scope.bedroomOpts = [{number: 1}, {number: 2}, {number: 3}, {number: 4}, {number: 5}];
+	$scope.bathroomOpts = [{number: 1}, {number: 2}, {number: 3}, {number: 4}, {number: 5}];
 });
